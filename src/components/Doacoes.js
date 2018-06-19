09e9b0e496ad3996668bd181d293e2f56909cc80
douglasfelipebs <douglasfelipebs@hotmail.com>
2018-06-19 17:43:11 -0300

    Doacoes
    Trocar

--- src/components/Doacoes.js
+++ src/components/Doacoes.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react'
+import { botaoPag, textoDoacao} from './Layout.css'
 
 class Doacoes extends Component {
 
     constructor(props) {
         super(props)
+
+        this.state = {
+            dumpIpmsum: [
+                'Atualmente a equipe dos bombeiros de Ibirama é composta por 30 integrantes, ',
+                'dos quais 12 são efetivos e 18 são voluntários.',
+                'Devido ao grande numero de voluntários e verbas que nem sempre são suficientes ',
+                'para atualizarmos os equipamentos, contamos com a ajuda da população por meio ',
+                'de doações para podermos atende-los melhor. A doação pode ser de qualquer valor e será ',
+                'de grande ajuda para o Corpo de Bombeiros de Ibirama. Para fazer a sua doação basta  ',
+                'clicar no botão abaixo e seguir as intruções.'
+            ]
+        }
     }
 
     render() {
         return (
-            <div>
-                <form action="https://pagseguro.uol.com.br/checkout/v2/donation.html" method="post">
-                    <input type="hidden" name="currency" value="BRL" />
-                    <input type="hidden" name="receiverEmail" value="bombeirosibirama@ibnet.com.br" />
-                    <input type="hidden" name="iot" value="button" />
-                    <input type="image" src="https://stc.pagseguro.uol.com.br/public/img/botoes/doacoes/205x30-doar.gif" name="submit" alt="Pague com PagSeguro - é rápido, grátis e seguro!" />
-                </form>
+            <div className={botaoPag}>
+                <p className={textoDoacao}>
+                    {this.state.dumpIpmsum}
+                </p>
+                <div>
+                    <form action="https://pagseguro.uol.com.br/checkout/v2/donation.html" method="post">
+                        <input type="hidden" name="currency" value="BRL" />
+                        <input type="hidden" name="receiverEmail" value="bombeirosibirama@ibnet.com.br" />
+                        <input type="hidden" name="iot" value="button" />
+                        <input type="image" src="https://stc.pagseguro.uol.com.br/public/img/botoes/doacoes/205x30-doar.gif" name="submit" alt="Pague com PagSeguro - é rápido, grátis e seguro!" />
+                    </form>
+                </div>
             </div>
         )
     }
